be85efa43dabb64519b3e8f78a84f6588305a2e2
artur <asuilin@gmail.com>
2019-01-05 00:15:48 +0300

    conversion-related articles

--- bibtoyaml.sh
+++ bibtoyaml.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-~/miniconda3/bin/pandoc-citeproc -y bib/gender_age.bib > data/bibrefs/gender_age.yaml
+#~/miniconda3/bin/pandoc-citeproc -y bib/gender_age.bib > data/bibrefs/gender_age.yaml
+~/miniconda3/bin/pandoc-citeproc -y bib/mab.bib > data/bibrefs/mab.yaml
